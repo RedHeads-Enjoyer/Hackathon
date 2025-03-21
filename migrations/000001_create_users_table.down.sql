9f51be8fa5ca3ffb11a5c55216bd1e03891634a2
alosev <alosev@molnet.ru>
2025-03-22 02:04:00 +0300

    Реализован механизм миграции

--- migrations/000001_create_users_table.down.sql
+++ migrations/000001_create_users_table.down.sql
@@ -1,6 +1,1 @@
-CREATE TABLE users (
-                       id SERIAL PRIMARY KEY,
-                       email VARCHAR(255) UNIQUE NOT NULL,
-                       password VARCHAR(255) NOT NULL,
-                       created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
+DROP TABLE IF EXISTS users;
--- migrations/000001_create_users_table.up.sql
+++ migrations/000001_create_users_table.up.sql
@@ -1,1 +1,6 @@
-DROP TABLE users;
+CREATE TABLE users (
+                       id SERIAL PRIMARY KEY,
+                       email VARCHAR(255) UNIQUE NOT NULL,
+                       password VARCHAR(255) NOT NULL,
+                       created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
