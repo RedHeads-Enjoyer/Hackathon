b696921d5d885e9adaee487c82122686ac37097c
alosev <alosev@molnet.ru>
2025-05-25 15:16:08 +0300

    Улучшил стили

--- server/models/DTO/organizationDTO/organizationCreate.go
+++ server/models/DTO/organizationDTO/organizationCreate.go
@@ -3,11 +3,11 @@ package organizationDTO
 import "server/models"
 
 type OrganizationCreate struct {
-	LegalName    string `json:"legalName" validate:"required"`
+	LegalName    string `json:"legalName" validate:"required,max=255"`
 	INN          string `json:"INN" validate:"required,len=12"`
 	OGRN         string `json:"OGRN,required,len=13"`
-	ContactEmail string `json:"contactEmail" validate:"required,email"`
-	Website      string `json:"website,required"`
+	ContactEmail string `json:"contactEmail" validate:"required,email,max=100"`
+	Website      string `json:"website,required,max=100"`
 }
 
 func (dto *OrganizationCreate) ToModel(userID uint) *models.Organization {
